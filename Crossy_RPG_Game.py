1aff5a4875003df5bdae3acbabbed6885a2904c4
Mariah Hall <hallmar94@gmail.com>
2019-06-22 22:36:09 -0400

    add enemy character and boundary checks

--- Crossy_RPG_Game.py
+++ Crossy_RPG_Game.py
@@ -1,49 +1,123 @@
+# Gain access to pygame library
 import pygame
 
-pygame.init()
-
-#Size of the screen
+# Screen info for running pygame
+SCREEN_TITLE = 'Crossy RPG'
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = 'Crossy RPG'
-# Colors according to RGB codes
 WHITE_COLOR = (255, 255, 255)   
 BLACK_COLOR = (0, 0, 0)
-# Clock used to update game events and frames
 clock = pygame.time.Clock()
-# set next iteration of clock (typical FPS is 60)
-TICK_RATE = 60
-is_game_over = False
 
-# Create the window of specified size in white to display the game
-game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-# Set the game window color to white
-game_screen.fill(WHITE_COLOR)
-pygame.display.set_caption(SCREEN_TITLE)
-
-player_image = pygame.image.load('images/player.png')
-player_image = pygame.transform.scale(player_image, (50, 50))
-
-# Main game loop, used to update all gameplay such as movement, checks and graphics
-while not is_game_over:
-
-    # Loop to get thru all events occuring at any given time
-    # Events are most often mouse movement, mouse and button clicks or exit events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            is_game_over = True
-            
-        print(event)
-
-    game_screen.blit(player_image, (275, 275))
-
-    # Draw rectangle on top of the game screen canvas (x, y, width, height)
-    # pygame.draw.rect(game_screen, BLACK_COLOR, [250, 250, 100, 100])
-    # Draw circle on top of game screen (x, y, radius)
-    # pygame.draw.circle(game_screen, BLACK_COLOR, (300, 200), 50)
-       
-    pygame.display.update()
-    clock.tick(TICK_RATE)
+class Game:
     
+    TICK_RATE = 60
+
+    # Initializer for the game class to set up title, width and height
+    def __init__(self, title, width, height):
+        self.title = title
+        self.width = width
+        self.height = height
+
+        # Create the window of specified size in white to display the game
+        self.game_screen = pygame.display.set_mode((width, height))
+        self.game_screen.fill(WHITE_COLOR)
+        pygame.display.set_caption(title)
+
+    def run_game_loop(self):
+        is_game_over = False
+        direction = 0
+
+        player_character = PlayerCharacter('images/player.png', 275, 500, 50, 50)
+        enemy_0 = EnemyCharacter('images/enemy0.png', 20, 300, 50, 50)
+        while not is_game_over:
+
+            for event in pygame.event.get():
+                # Allows exit of game when that type is detected
+                if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+                    is_game_over = True
+                # Detects when key is pressed down
+                elif event.type == pygame.KEYDOWN:
+                    # Move up if up key pressed
+                    if event.key == pygame.K_UP:
+                        direction = 1
+                    # Move down if down key pressed
+                    elif event.key == pygame.K_DOWN:
+                        direction = -1
+                # Detect when key is released and stop movement
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        direction = 0
+                print(event)
+                
+            self.game_screen.fill(WHITE_COLOR)
+            
+            player_character.move(direction, self.height)
+            player_character.draw(self.game_screen)
+
+            enemy_0.move(self.width)
+            enemy_0.draw(self.game_screen)
+            
+            pygame.display.update()
+            clock.tick(self.TICK_RATE)
+            
+class GameObject:
+
+    def __init__(self, image_path, x, y, width, height):
+        object_image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(object_image, (width, height))
+        
+        self.x_pos = x
+        self.y_pos = y
+
+        self.width = width
+        self.height = height
+        
+    def draw(self, background):
+        background.blit(self.image, (self.x_pos, self.y_pos))
+
+class PlayerCharacter(GameObject):
+
+    SPEED = 10
+    
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    # Move function to move character up or down     
+    def move(self, direction, max_height):
+        if direction > 0:
+            self.y_pos -= self.SPEED
+        elif direction < 0:
+            self.y_pos += self.SPEED
+
+        # Make sure the character never goes past the bottom of the screen    
+        if self.y_pos >= max_height - 50:
+            self.y_pos = max_height - 50
+
+# Class to represent enemies moving left to right and right to left
+class EnemyCharacter(GameObject):
+
+    SPEED = 10
+    
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    # Move function will move character right once it hits too far on left
+    # and left once it hits too far right on screen
+    def move(self, max_width):
+        if self.x_pos <= 10:
+            self.SPEED = abs(self.SPEED)
+        elif self.x_pos >= max_width - 30:
+            self.SPEED = -abs(self.SPEED)
+        self.x_pos += self.SPEED
+        
+pygame.init()
+
+# create a new instance of a Game
+new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
+new_game.run_game_loop()
+
+
+# Quit game  
 pygame.quit()
 quit()
